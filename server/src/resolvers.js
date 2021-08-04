3e10747f9eca4d68494c92b36beb417ec9d7a6fd
kaindy7633 <kaindy7633@gmail.com>
2021-08-04 18:04:18 +0800

    添加getTrack

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,6 +5,18 @@ const resolvers = {
     // tracksForHome: (parent, args ,context, info) => {},
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
+    },
+
+    // get a single track by ID, for the track page
+    track: async (_, { id }, { dataSources }) => {
+      // get tarck detail
+      const track = dataSources.trackAPI.getTrack(id);
+
+      // get module details for the track
+      const modules = await dataSources.trackAPI.getTrackModules(id);
+
+      // shape the data in the way that the schema expects it
+      return {...track, modules};
     }
   },
 
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   # Schema definitions go here
   type Query {
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   type Track {
@@ -16,12 +17,27 @@ const typeDefs = gql`
     thumbnail: String
     length: Int
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   type Author {
     id: ID!
     name: String!
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 
